2374ccad3aa5e001e1becb348e49b4ff1b22d890
spmcdonough <sean@bistreamfoundry.com>
2014-08-31 11:55:21 -0400

    Initial commit of solution.

---
+++ AOPinSharePoint/AopExamplePart/AopPart.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Web.UI;
+using System.Web.UI.WebControls.WebParts;
+
+
+namespace AOPinSharePoint.AopPart
+{
+
+    
+    [ToolboxItemAttribute(false)]
+    public class AopPart : WebPart
+    {
+        // Visual Studio might automatically update this path when you change the Visual Web Part project item.
+        private const string _ascxPath = @"~/_CONTROLTEMPLATES/15/AOPinSharePoint/AopPart/AopPartUserControl.ascx";
+
+        protected override void CreateChildControls()
+        {
+            Control control = Page.LoadControl(_ascxPath);
+            Controls.Add(control);
+        }
+    }
+
+
+}
---
+++ AOPinSharePoint/AopExamplePart/AopPartUserControl.ascx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+
+
+namespace AOPinSharePoint.AopPart
+{
+    public partial class AopPartUserControl : UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void Example01Button_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
